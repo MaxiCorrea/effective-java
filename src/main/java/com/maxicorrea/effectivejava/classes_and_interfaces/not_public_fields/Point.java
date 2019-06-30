3a21f5f63e7451d36a577881366bdeef2e7c0b83
Maximiliano Correa <jmaxicorrea@gmail.com>
2019-06-30 18:31:52 -0300

    Not Public Fields.

---
+++ src/main/java/com/maxicorrea/effectivejava/classes_and_interfaces/not_public_fields/Point.java
@@ -0,0 +1,12 @@
+package com.maxicorrea.effectivejava.classes_and_interfaces.not_public_fields;
+/**
+ * 
+ * @author mxcorrea
+ *
+ */
+public class Point {
+
+	public double x;
+	public double y;
+	
+}
