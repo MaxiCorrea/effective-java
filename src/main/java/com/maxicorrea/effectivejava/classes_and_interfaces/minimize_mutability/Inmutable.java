830ff14c259dcb05ebd18fb1e32c52708b4ef054
Maximiliano Correa <jmaxicorrea@gmail.com>
2019-09-28 15:53:49 -0300

    Minimize Mutability Example

---
+++ src/main/java/com/maxicorrea/effectivejava/classes_and_interfaces/minimize_mutability/Inmutable.java
@@ -0,0 +1,27 @@
+package com.maxicorrea.effectivejava.classes_and_interfaces.minimize_mutability;
+/**
+ * 
+ * @author mxcorrea
+ *
+ */
+public class Inmutable {
+
+	private String att1;
+	private String att2;
+	
+	public Inmutable(
+			final String att1 ,
+			final String att2) {
+		this.att1 = att1;
+		this.att2 = att2;
+	}
+	
+	public String getAtt1() {
+		return att1;
+	}
+	
+	public String getAtt2() {
+		return att2;
+	}
+	
+}
