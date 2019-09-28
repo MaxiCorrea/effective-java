0c3abc6247fbc0a62c70c9552b245caee95eaf70
Maximiliano Correa <jmaxicorrea@gmail.com>
2019-09-28 16:37:40 -0300

    Ejemplo de Objeto inmutable

---
+++ src/main/java/com/maxicorrea/effectivejava/classes_and_interfaces/minimize_mutability/ComplexNumber.java
@@ -0,0 +1,65 @@
+package com.maxicorrea.effectivejava.classes_and_interfaces.minimize_mutability;
+/**
+ * Inmutable class Example
+ * @author mxcorrea
+ *
+ */
+public final class ComplexNumber {
+
+	private final double re;
+	private final double im;
+	
+	public ComplexNumber (
+			final double re ,
+			final double im) {
+		this.re = re;
+		this.im = im;
+	}
+	
+	public double realPart() {
+		return re;
+	}
+	
+	public double imaginaryPart() {
+		return im;
+	}
+	
+	public ComplexNumber plus(
+			final ComplexNumber another) {
+		double localRe = re + another.re;
+		double localim = im + another.im;
+		return new ComplexNumber(localRe, localim);
+	}
+	
+	public ComplexNumber minus(
+			final ComplexNumber another) {
+		double localRe = re - another.re;
+		double localIm =  im - another.im;
+		return new ComplexNumber(localRe, localIm);
+	}
+	
+	
+	public ComplexNumber times(
+			final ComplexNumber another) {
+		double localRe = re * another.re - im * another.im;
+		double localIm = re * another.im + im * another.re;
+		return new ComplexNumber(localRe, localIm);
+	}
+		
+	@Override 
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (!(o instanceof ComplexNumber))
+			return false;
+		ComplexNumber c = (ComplexNumber) o;
+		return Double.compare(c.re, re) == 0
+			&& Double.compare(c.im, im) == 0;
+	}
+	
+	@Override 
+	public String toString() {
+		return "(" + re + " + " + im + "i)";
+	}
+	
+}
