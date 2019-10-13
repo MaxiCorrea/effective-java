056d1c29e6eb06d88aacbd4ad5a72a5fc4c08a7d
Maximiliano Correa <jmaxicorrea@gmail.com>
2019-10-12 22:24:14 -0300

    Declaracion de interface con solo constantes

---
+++ src/main/java/com/maxicorrea/effectivejava/classes_and_interfaces/use_interfaces_only_to_define_types/PhysicalConstants.java
@@ -0,0 +1,17 @@
+package com.maxicorrea.effectivejava.classes_and_interfaces.use_interfaces_only_to_define_types;
+/**
+ * 
+ * Patron de "Interface con solo declaracino de Constantes"
+ * 
+ * @author mxcorrea
+ *
+ */
+public interface PhysicalConstants {
+
+	double AVOGADROS_NUMBER = 6.022_140_857e23;
+	
+	double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+	
+	double ELECTRON_MASS	= 9.109_383_56e-31;
+	
+}
