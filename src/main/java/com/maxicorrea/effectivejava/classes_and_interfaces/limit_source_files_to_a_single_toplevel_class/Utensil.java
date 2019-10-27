34f5c7c1593c28efd6aa660dafb762e2b3646620
Maximiliano Correa <jmaxicorrea@gmail.com>
2019-10-27 00:28:03 -0300

    Mala Practica (Definicion de Clases de nivel superior en un mismo Suurce File)

---
+++ src/main/java/com/maxicorrea/effectivejava/classes_and_interfaces/limit_source_files_to_a_single_toplevel_class/Utensil.java
@@ -0,0 +1,13 @@
+package com.maxicorrea.effectivejava.classes_and_interfaces.limit_source_files_to_a_single_toplevel_class;
+/**
+ * No es puede agregar esta clase porque ya esta definida!
+ * ¿Pero en donde? 
+ * No la veo en los archivos fuente . OK (Busca clase por clase en tu proyecto)
+ * @author mxcorrea
+ *
+ */
+
+// The type Utensil is already defined
+/*public class Utensil {
+
+}*/
