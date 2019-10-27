d70a0026b946a268cf29bb280c37ee7d0d3ca21e
Maximiliano Correa <jmaxicorrea@gmail.com>
2019-10-27 01:52:02 -0300

    Forma mas simple de crear una enumeracion.

---
+++ src/main/java/com/maxicorrea/effectivejava/enums_and_annotations/use_enums_instead_of_int_constants/EnumConstants.java
@@ -0,0 +1,12 @@
+package com.maxicorrea.effectivejava.enums_and_annotations.use_enums_instead_of_int_constants;
+/**
+ * Ejemplo de la forma mas simple de crear una enumeracion.
+ * @author mxcorrea
+ *
+ */
+public class EnumConstants {
+
+	public enum Apple { FUJI, PIPPIN, GRANNY_SMITH }
+	public enum Orange { NAVEL, TEMPLE, BLOOD }
+	
+}
