41f6fdb830a973d4040c9a754b7995109ce96de4
Maximiliano Correa <jmaxicorrea@gmail.com>
2019-10-27 01:47:22 -0300

    Int Enum Pattern.
    Los programaso que utlizan "Int Enum Pattern" son Fragiles.
    
    Las constantes se compilan en el cliente que los usa.
    No hay manejar facil de imprimir las constantes a Strings.
    No es Util al mosmento de depurar programas.
    No hay menera confuable de iteras sobre los elementos.
    No hay manera confiable de saber la cantidad de elementos enumerados.

---
+++ src/main/java/com/maxicorrea/effectivejava/enums_and_annotations/use_enums_instead_of_int_constants/IntConstants.java
@@ -0,0 +1,14 @@
+package com.maxicorrea.effectivejava.enums_and_annotations.use_enums_instead_of_int_constants;
+/**
+ * Implementacion de el patron conocido como "Int Enum Pattern" (Smell Code)
+ * @author mxcorrea
+ *
+ */
+public class IntConstants {
+
+	public static final int INT_CONSTANT_1 = 234;
+	public static final int INT_CONSTANT_2 = 334;
+	public static final int INT_CONSTANT_3 = 434;
+	public static final int INT_CONSTANT_4 = 634;
+
+}
